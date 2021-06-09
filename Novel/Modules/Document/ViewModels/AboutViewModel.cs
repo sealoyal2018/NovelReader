374759ad041c31d3a4118cc380e33291f17a710d
rsdte <rsdte@foxmail.com>
2021-06-09 22:55:08 +0800

    添加关于软件界面

---
+++ Novel/Modules/Document/ViewModels/AboutViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novel.Modules.Document.ViewModels {
+    [Export(typeof(IDocument))]
+    public class AboutViewModel : IDocument {
+        public string Name {
+            get {
+                return "关于软件";
+            }
+        }
+
+        public string Icon {
+            get {
+                return string.Empty;
+            }
+        }
+
+        public bool Show {
+            get {
+                return true;
+            }
+        }
+
+        public int Index {
+            get {
+                return 10;
+            }
+        }
+
+        /// <summary>
+        /// 检查更新
+        /// </summary>
+        /// <returns></returns>
+        public Task CheckUpdateAsync() {
+
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
